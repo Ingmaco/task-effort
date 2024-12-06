940d8a4188b3bf093bda9b26818e3bfb5bb6a4a0
Ingmaco <152733564+Ingmaco@users.noreply.github.com>
2024-12-06 17:33:52 +0100

    Revert "dataconnect"
    This reverts commit 8588044bb17df572784dec9b480b8373fe483c8e.

--- dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,8 +0,0 @@
-import { getDataConnect, validateArgs } from 'firebase/data-connect';
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'task-effort',
-  location: 'us-central1'
-};
-
